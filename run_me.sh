bc259f6621dee1d3bdbf43873eb2f97014cd35bd
António Lima <tocalagarto93@gmail.com>
2016-05-19 12:09:15 +0100

    started creating python scripts as well

--- run_me.sh
+++ run_me.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # get posts from facebook group
+python get_posts.py
 
 # generate newsletter from facebook group
+python generate_newsletter.py
 
 # send newsletter to mailing list
+python send_newsletter.py
 
 #EOF
